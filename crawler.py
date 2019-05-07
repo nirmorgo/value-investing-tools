6a580fadd2eef99398952bc9f7ec2b22bd21151a
nirmorgo <nirmorgo@gmail.com>
2019-05-08 00:26:28 +0300

    added option to scrape for xbrl files

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 # This script will download all the 10-K, 10-Q and 8-K
 # provided that of company symbol and its cik code.
-# shamelessly borrowed from https://github.com/coyo8/sec-edgar
+# shamelessly borrowed from https://github.com/coyo8/sec-edgar and slightly modified 
+# so it can scrape for xbrl files instead of txt
 
 from __future__ import print_function  # Compatibility with Python 2
 
@@ -9,9 +10,11 @@ import os
 import errno
 from bs4 import BeautifulSoup
 import datetime
+from ipdb import set_trace
+import re
 
 DEFAULT_DATA_PATH = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..', 'SEC-Edgar-Data'))
+    os.path.dirname(__file__), '.', 'SEC-Edgar-Data'))
 
 
 class SecCrawler(object):
@@ -45,8 +48,23 @@ class SecCrawler(object):
             with open(path, "ab") as f:
                 f.write(data.encode('ascii', 'ignore'))
 
-    @staticmethod
-    def _create_document_list(data):
+    def _find_xbrl_link(self, base_url):
+        with requests.get(base_url) as r:
+            data = r.text
+        soup = BeautifulSoup(data, features='html.parser')
+        # store the link in the list
+        link_list = [link.string for link in soup.find_all('a')]
+        regex = re.compile('.*[0-9].xml')
+        for link in link_list:
+            if  link is not None and regex.match(link):
+                file_name = link
+                file_url = '/'.join(base_url.split('/')[:-1] + [link])
+
+
+        # set_trace()
+        return file_url, file_name
+    
+    def _create_document_list(self, data, doc_type='txt'):
         # parse fetched data using beatifulsoup
         # Explicit parser needed
         soup = BeautifulSoup(data, features='html.parser')
@@ -57,14 +75,22 @@ class SecCrawler(object):
         print("Starting download...")
 
         # List of url to the text documents
-        txt_urls = [link[:link.rfind("-")] + ".txt" for link in link_list]
-        # List of document doc_names
-        doc_names = [url.split("/")[-1] for url in txt_urls]
+        if doc_type == 'txt':
+            txt_urls = [link[:link.rfind("-")] + ".txt" for link in link_list]
+            # List of document doc_names
+            doc_names = [url.split("/")[-1] for url in txt_urls]
+            return list(zip(txt_urls, doc_names))
 
-        return list(zip(txt_urls, doc_names))
+        elif doc_type == 'xbrl':
+            xbrl_urls, doc_names = [], []
+            for link in link_list:
+                xbrl_url, doc_name = self._find_xbrl_link(link)
+                xbrl_urls.append(xbrl_url)
+                doc_names.append(doc_name)
+                # set_trace()
+            return list(zip(xbrl_urls, doc_names))
 
-    @staticmethod
-    def _sanitize_date(date):
+    def _sanitize_date(self, date):
         if isinstance(date, datetime.datetime):
             return date.strftime("%Y%m%d")
         elif isinstance(date, str):
@@ -74,7 +100,7 @@ class SecCrawler(object):
             if date < 10**7 or date > 10**8:
                 raise TypeError('Date must be of the form YYYYMMDD')
 
-    def _fetch_report(self, company_code, cik, priorto, count, filing_type):
+    def _fetch_report(self, company_code, cik, priorto, count, filing_type, doc_type='txt'):
         priorto = self._sanitize_date(priorto)
         self._make_directory(company_code, cik, priorto, filing_type)
 
@@ -84,11 +110,11 @@ class SecCrawler(object):
                   'CIK': cik, 'type': filing_type, 'dateb': priorto, 'count': count}
         print("started {filing_type} {company_code}".format(
             filing_type=filing_type, company_code=company_code))
-        r = requests.get(base_url, params=params)
-        data = r.text
+        with requests.get(base_url, params=params) as r:
+            data = r.text
 
         # get doc list data
-        docs = self._create_document_list(data)
+        docs = self._create_document_list(data, doc_type)
 
         try:
             self._save_in_directory(
@@ -101,8 +127,8 @@ class SecCrawler(object):
     def filing_10Q(self, company_code, cik, priorto, count):
         self._fetch_report(company_code, cik, priorto, count, '10-Q')
 
-    def filing_10K(self, company_code, cik, priorto, count):
-        self._fetch_report(company_code, cik, priorto, count, '10-K')
+    def filing_10K(self, company_code, cik, priorto, count, doc_type='txt'):
+        self._fetch_report(company_code, cik, priorto, count, '10-K', doc_type)
 
     def filing_8K(self, company_code, cik, priorto, count):
         self._fetch_report(company_code, cik, priorto, count, '8-K')
