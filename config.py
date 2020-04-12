7b59055171e9c6889c430ba50c6d941793a1c73d
Nir Morgulis <nirmorgo@gmail.com>
2020-04-12 13:45:26 +0300

    Delete config.py
    removed config file

--- config.py
+++
@@ -1,4 +0,0 @@
-last_10k_api_key = 'c8c10f945be94992b7147f8ab37fb909'
-quandle_api_key = 'VUWwZVZRSnCYmvD5thf3'
-simfin_api_key = '4t4r87ebiM862u7prNfiPMmlTjK02rB3'
-WTD_api_key = 'VGMVGDdhs4MpaUFEau1AWwzghD32XPbWAQYExjeoHwEz7XGcwdttxLitIPCe'
